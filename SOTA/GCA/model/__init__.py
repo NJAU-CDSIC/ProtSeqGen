253c7132a53eda3206cd6e427a3c503c40d520ea
NJAU-CDSIC <142595806+NJAU-CDSIC@users.noreply.github.com>
2025-10-13 19:05:32 +0800

    Delete SOTA directory

--- SOTA/GCA/model/__init__.py
+++
@@ -1,4 +0,0 @@
-from .struct2seq import Struct2Seq
-from .gca import GCA
-
-__all__ = ['Struct2Seq', 'GCA']
--- SOTA/PiFold/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .main_utils import *
-# from .alphadesign_utils import gather_nodes, _dihedrals,  _rbf, _orientations_coarse_gl
-from .simdesign_utils import _get_rbf, _orientations_coarse_gl_tuple, gather_nodes, _dihedrals,  _rbf, _orientations_coarse_gl
